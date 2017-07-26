66517fe1408e668cbb5560f4e8f83cd9be85684b
Lavinia Medeiros <laviniameds@icloud.com>
2017-07-25 23:15:41 -0300

    conserto de bugs css

--- js/mask.js
+++ js/mask.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
   $('#cpf').mask('000.000.000-00');
   $('#rg').mask('000.000.000');
   $('#cep').mask('00000-000');
+  $('#numero-cartao').mask('0000 0000 0000 0000 - 000');
 });
